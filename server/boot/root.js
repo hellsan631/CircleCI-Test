375bf35fe04bf224e38dd56b07073c90726695e3
Mathew Kleppin <matkle414@gmail.com>
2016-04-20 19:59:48 -0500

    add client and test

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,15 @@
+var path = require('path');
+
 module.exports = function(server) {
+
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+
+  var staticPath = path.resolve(__dirname + '/../../client');
+
+  router.get('/', function(req, res) {
+    res.sendFile(staticPath + '/index.html');
+  });
+
   server.use(router);
 };
---
+++ test/passme.js
@@ -0,0 +1,5 @@
+import test from 'ava';
+
+test('Passing Test', t => {
+  t.pass();
+});
